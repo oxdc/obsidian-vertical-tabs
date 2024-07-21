1f291a541283fcf52ec353c132b4dc7bc0d84ec8
oxdc <29519076+oxdc@users.noreply.github.com>
2024-07-21 18:20:49 +0800

    default map, refresh layout

---
+++ default_map.ts
@@ -0,0 +1,18 @@
+export default class DefaultMap<K, V> extends Map<K, V> {
+	factory: () => V;
+
+	constructor(
+		factory: () => V,
+		entries?: readonly (readonly [K, V])[] | null
+	) {
+		super(entries);
+		this.factory = factory;
+	}
+
+	get(key: K): V {
+		if (!this.has(key)) {
+			this.set(key, this.factory());
+		}
+		return super.get(key) ?? this.factory();
+	}
+}
--- main.ts
+++ main.ts
@@ -1,134 +1,43 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, WorkspaceLeaf, WorkspaceParent } from "obsidian";
+import DefaultMap from "default_map";
 
-// Remember to rename these classes and interfaces!
+type GroupID = string;
 
-interface MyPluginSettings {
-	mySetting: string;
+interface WorkspaceParentWithId extends WorkspaceParent {
+	id?: GroupID;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFUALT_GROUP_ID = "default";
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ObsidianVerticalTabs extends Plugin {
+	groupedLeaves: DefaultMap<GroupID, WorkspaceLeaf[]>;
 
 	async onload() {
-		await this.loadSettings();
+		this.refreshLeaves();
+		this.registerEvents();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Play",
+			(evt: MouseEvent) => {
+				console.log(this.groupedLeaves);
 			}
+		);
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
+	}
+
+	async registerEvents() {
+		this.app.workspace.on("layout-change", this.refreshLeaves);
+	}
+
+	async refreshLeaves() {
+		this.groupedLeaves = new DefaultMap(() => []);
+		this.app.workspace.iterateRootLeaves((leaf: WorkspaceLeaf) => {
+			const parent = leaf.parent as WorkspaceParentWithId;
+			const parentId: GroupID = parent.id || DEFUALT_GROUP_ID;
+			this.groupedLeaves.get(parentId).push(leaf);
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	onunload() {}
 }
