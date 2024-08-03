198ddab2c0ad5f2d8b1e0762d0fb671ee590b407
oxdc <29519076+oxdc@users.noreply.github.com>
2024-08-03 16:06:35 +0800

    tweak: Droppable el behavior when dragging

--- src/components/Droppable.tsx
+++ src/components/Droppable.tsx
@@ -1,6 +1,7 @@
 import { useDroppable } from "@dnd-kit/core";
 import { TabSlot } from "./TabSlot";
 import * as VT from "../models/VTWorkspace";
+import { CssClasses, toClassName } from "src/utils/CssClasses";
 
 export interface DroppableProps {
 	id: VT.Identifier;
@@ -11,8 +12,12 @@ export const Droppable = (props: DroppableProps) => {
 	const { setNodeRef, isOver, active } = useDroppable({
 		id: props.id,
 	});
+	const droppableElClasses: CssClasses = {
+		"tab-droppable": true,
+		"is-dragging-self": active?.id == props.id,
+	};
 	return (
-		<div ref={setNodeRef} className="tab-droppable">
+		<div ref={setNodeRef} className={toClassName(droppableElClasses)}>
 			{isOver && <TabSlot isHidden={active?.id == props.id} />}
 			{props.children}
 		</div>
