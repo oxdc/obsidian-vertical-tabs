f74aaf1f07539089dc3036b9faa227ca2b1b2a60
oxdc <29519076+oxdc@users.noreply.github.com>
2024-08-03 16:01:14 +0800

    updated styles

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -7,11 +7,16 @@ export interface DraggableProps {
 }
 
 export const Draggable = (props: DraggableProps) => {
-	const { attributes, listeners, setNodeRef } = useDraggable({
+	const { attributes, listeners, setNodeRef, transform } = useDraggable({
 		id: props.id,
 	});
+	const style = {
+		transform: transform
+			? `translate3d(0, ${transform.y}px, 0)`
+			: undefined,
+	};
 	return (
-		<div ref={setNodeRef} {...listeners} {...attributes}>
+		<div ref={setNodeRef} style={style} {...listeners} {...attributes}>
 			{props.children}
 		</div>
 	);
