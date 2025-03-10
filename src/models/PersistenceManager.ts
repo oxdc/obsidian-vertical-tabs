69196a8968acdd15bb30758f78137db65858f669
oxdc <29519076+oxdc@users.noreply.github.com>
2025-03-10 20:23:24 +0800

    updated persistence manager

--- src/models/PersistenceManager.ts
+++ src/models/PersistenceManager.ts
@@ -1,17 +1,19 @@
 import { nanoid } from "nanoid";
-import { App } from "obsidian";
+import { App, PluginManifest } from "obsidian";
 
 const DEVICE_ID_KEY = "vertical-tabs:device-id";
 
 export class PersistenceManager {
 	readonly installationID: string;
 	readonly deviceID: string;
+	readonly manifest: PluginManifest;
 	readonly app: App;
 
-	constructor(app: App, installationID: string) {
+	constructor(app: App, installationID: string, manifest: PluginManifest) {
 		this.app = app;
 		this.installationID = installationID;
 		this.deviceID = this.loadDeviceID();
+		this.manifest = manifest;
 	}
 
 	private loadDeviceID() {
@@ -23,34 +25,75 @@ export class PersistenceManager {
 		return deviceID;
 	}
 
+	private normalizePath(path: string) {
+		path = path.replace(/([\\/])+/g, "/");
+		path = path.replace(/(^\/+|\/+$)/g, "");
+		path = path === "" ? "/" : path;
+		path = path.replace(/\u00A0|\u202F/g, " ");
+		return path;
+	}
+
+	private constructSafeFilePathFrom(key: string) {
+		const defaultDir = `${this.app.vault.configDir}/plugins/${this.manifest.id}`;
+		const dir = this.manifest.dir ?? defaultDir;
+		const unsafeFilePath = `${dir}/${key}.json`;
+		const normalizedPath = this.normalizePath(unsafeFilePath);
+		return normalizedPath.normalize("NFC");
+	}
+
+	private async writeObjectToFile<T>(key: string, value: T) {
+		const filePath = this.constructSafeFilePathFrom(key);
+		await this.app.vault.writeJson(filePath, value as object);
+	}
+
+	private async readObjectFromFile<T>(key: string): Promise<T | null> {
+		const filePath = this.constructSafeFilePathFrom(key);
+		return this.app.vault.readJson(filePath) as T | null;
+	}
+
+	private async hasFile(key: string): Promise<boolean> {
+		const filePath = this.constructSafeFilePathFrom(key);
+		return this.app.vault.exists(filePath);
+	}
+
 	get prefix() {
 		return `vertical-tabs:${this.installationID}:${this.deviceID}`;
 	}
 
-	get<T>(key: string) {
+	async get<T>(key: string): Promise<T | null> {
 		const value = localStorage.getItem(`${this.prefix}:${key}`);
-		if (!value) return null;
-		return JSON.parse(value) as T;
-	}
-
-	set<T>(key: string, value: T) {
-		localStorage.setItem(`${this.prefix}:${key}`, JSON.stringify(value));
-	}
-
-	remove(key: string) {
-		localStorage.removeItem(`${this.prefix}:${key}`);
-	}
-
-	has(key: string) {
-		return localStorage.getItem(`${this.prefix}:${key}`) !== null;
-	}
-
-	clear() {
-		for (let i = 0; i < localStorage.length; i++) {
-			const key = localStorage.key(i);
-			if (key?.startsWith(this.prefix)) {
-				localStorage.removeItem(key);
-			}
+		if (value) {
+			return JSON.parse(value) as T;
 		}
+		const fileExists = await this.hasFile(key);
+		if (fileExists) {
+			return await this.readObjectFromFile<T>(key);
+		}
+		return null;
+	}
+
+	async set<T>(key: string, value: T, largeBlob = false) {
+		if (largeBlob) {
+			await this.writeObjectToFile(key, value);
+		} else {
+			localStorage.setItem(
+				`${this.prefix}:${key}`,
+				JSON.stringify(value)
+			);
+		}
+	}
+
+	async remove(key: string) {
+		localStorage.removeItem(`${this.prefix}:${key}`);
+		const filePath = this.constructSafeFilePathFrom(key);
+		const file = this.app.vault.getFileByPath(filePath);
+		if (file) await this.app.vault.delete(file);
+	}
+
+	async has(key: string): Promise<boolean> {
+		return (
+			localStorage.getItem(`${this.prefix}:${key}`) !== null ||
+			(await this.hasFile(key))
+		);
 	}
 }
