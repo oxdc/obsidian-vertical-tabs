c6298ee519a7736ce27479fdf3836b0b46af5670
oxdc <29519076+oxdc@users.noreply.github.com>
2025-07-23 11:29:23 +0800

    Cleaned up code structure for Tab (wip)

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -28,6 +28,8 @@ import { HistoryBrowserModal } from "src/views/HistoryBrowserModal";
 import { getOpenFileOfLeaf } from "src/services/GetTabs";
 import { GroupViewType, setGroupViewType } from "src/models/VTGroupView";
 import { REFRESH_TIMEOUT, REFRESH_TIMEOUT_LONG } from "src/constants/Timeouts";
+import { byPinned } from "src/services/SortTabs";
+
 interface TabProps {
 	leaf: WorkspaceLeaf;
 	index: number;
@@ -36,16 +38,35 @@ interface TabProps {
 	viewType?: GroupViewType;
 }
 
-export const Tab = ({
-	leaf,
-	index,
-	isLast,
-	isSingleGroup,
-	viewType,
-}: TabProps) => {
+type DivMouseEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
+
+export const Tab = (props: TabProps) => {
 	const plugin = usePlugin();
 	const app = plugin.app;
 	const workspace = app.workspace;
+
+	const { leaf, index, isLast, isSingleGroup, viewType } = props;
+
+	/* Relavent settings */
+	const enableTabZoom = useSettings((state) => state.enableTabZoom);
+	const alwaysOpenInNewTab = useSettings((state) => state.alwaysOpenInNewTab);
+	const isEditingTabs = useViewState((state) => state.isEditingTabs);
+	const sortStrategy = tabCacheStore((state) => state.sortStrategy);
+
+	/* External states (managed by Obsidian) */
+	const leafPinnedState = leaf.getViewState().pinned ?? false;
+	const isWebViewer = leaf.view.getViewType() === "webviewer";
+
+	/* Internal states (managed by the component) */
+	const [isPinned, setIsPinned] = useState(leafPinnedState);
+	const [isEphemeral, setIsEphemeral] = useState(!!leaf.isEphemeral);
+	const [volatileTitle, setVolatileTitle] = useState<string | null>(null);
+
+	/* Store states (managed by zustand, shared by components) */
+	const lastActiveLeaf = useViewState((state) => state.latestActiveLeaf);
+
+	/* Actions (for mutating the shared store) */
+	const { refresh, sort } = tabCacheStore.getActions();
 	const {
 		bindPinningEvent,
 		bindEphemeralToggleEvent,
@@ -56,82 +77,60 @@ export const Tab = ({
 		mapViewCueIndex,
 		registerViewCueTab,
 	} = useViewState();
-	const [isPinned, setIsPinned] = useState(
-		leaf.getViewState().pinned ?? false
-	);
-	const [isEphemeral, setIsEphemeral] = useState(!!leaf.isEphemeral);
-	const { refresh, sort } = tabCacheStore.getActions();
-	const lastActiveLeaf = useViewState((state) => state.latestActiveLeaf);
-	const enableTabZoom = useSettings((state) => state.enableTabZoom);
-	const alwaysOpenInNewTab = useSettings((state) => state.alwaysOpenInNewTab);
-	const [volatileTitle, setVolatileTitle] = useState<string | null>(null);
-	const isWebViewer = leaf.view.getViewType() === "webviewer";
-	const isEditingTabs = useViewState((state) => state.isEditingTabs);
 
-	useEffect(() => {
-		if (!isWebViewer) setVolatileTitle(null);
-	}, [isWebViewer]);
-
-	const changePinnedState = (pinned: boolean) => {
-		setIsPinned(pinned);
-		if (pinned && leaf.isEphemeral) makeLeafNonEphemeral(leaf);
+	/* Commands */
+	/* Commands - Tab control */
+	const previewTab = (event: DivMouseEvent) => {
+		const file = getOpenFileOfLeaf(app, leaf);
+		if (file && ref.current) {
+			// Signal Obsidian to show the preview
+			workspace.trigger("hover-link", {
+				event: event.nativeEvent,
+				source: "vertical-tabs",
+				hoverParent: leaf,
+				targetEl: ref.current,
+				linktext: file.path,
+			});
+		}
 	};
-
-	useEffect(() => {
-		bindPinningEvent(leaf, changePinnedState);
-		bindEphemeralToggleEvent(app, leaf, (isEphemeral) => {
-			setIsEphemeral(isEphemeral);
-		});
-	}, [leaf.id]);
-
-	const togglePinned = () => {
-		leaf.togglePinned();
-		changePinnedState(leaf.getViewState().pinned ?? false);
-		sort();
-	};
-
-	const unPin = () => {
-		leaf.setPinned(false);
-		changePinnedState(false);
-		sort();
-	};
-
-	const closeTab = () => {
-		if (!leaf.getViewState().pinned) leaf.detach();
-	};
-
-	const midClickCloseTab = (
-		event: React.MouseEvent<HTMLDivElement, MouseEvent>
-	) => {
-		if (event.button === 1) closeTab();
-	};
-
+	// openTab activates the tab and scrolls it into view
 	const openTab = () => {
+		// Disable when editing tabs on mobile, preventing the sidebar from being closed
 		if (Platform.isMobile && isEditingTabs) return;
-		const positioning =
-			viewType === GroupViewType.MissionControlView ? "center" : "start";
+		// Signal the changes to Obsidian
 		const focus = viewType !== GroupViewType.MissionControlView;
 		workspace.setActiveLeaf(leaf, { focus });
 		workspace.onLayoutChange();
+		// Automatically unhide the group containing the tab
 		toggleHiddenGroup(leaf.parent.id, false);
+		// Lock the focus for Zen mode
 		lockFocusOnLeaf(app, leaf);
+		// Center the tab in the mission control view
+		const positioning =
+			viewType === GroupViewType.MissionControlView ? "center" : "start";
 		leaf.containerEl?.scrollIntoView({
 			behavior: "smooth",
 			block: positioning,
 			inline: "center",
 		});
 	};
-
-	const activeOrCloseTab = (
-		event: React.MouseEvent<HTMLDivElement, MouseEvent>
-	) => {
+	// closeTab detaches only unpinned tabs
+	const closeTab = () => {
+		if (!isPinned) leaf.detach();
+	};
+	// Convenience wrapper
+	const midClickCloseTab = (event: DivMouseEvent) => {
+		if (event.button === 1) closeTab();
+	};
+	// Alt-click closes the tab, otherwise opens it
+	const activeOrCloseTab = (event: DivMouseEvent) => {
 		if (event.altKey) {
 			closeTab();
 		} else {
 			openTab();
 		}
 	};
-
+	// Double clicking a tab makes it non-ephemeral and exits the mission control view
 	const makeLeafNonEphemeralAndExitMissionControl = () => {
 		makeLeafNonEphemeral(leaf);
 		if (viewType === GroupViewType.MissionControlView) {
@@ -139,14 +138,143 @@ export const Tab = ({
 		}
 	};
 
-	const menu = new Menu();
+	/* Commands - Pinning */
+	// setPinnedStates updates the states managed by the plugin,
+	// including internal and store states.
+	const setPinnedStates = (pinned: boolean) => {
+		// Internal state always reflects the change
+		setIsPinned(pinned);
+		// Tab becomes non-ephemeral if pinned
+		if (pinned && leaf.isEphemeral) makeLeafNonEphemeral(leaf);
+	};
+	// setPinned updates the states and fires events for side effects.
+	const setPinned = (pinned: boolean) => {
+		// Signal the change to Obsidian
+		leaf.setPinned(pinned);
+		// Update the states
+		setPinnedStates(pinned);
+		// Sort by pinned state, if specified by the user
+		if (sortStrategy?.compareFn === byPinned) sort();
+	};
+	// Convenience wrappers
+	const togglePinned = () => setPinned(!isPinned);
+	const unPin = () => setPinned(false);
 
+	/* Commands - History */
+	const addHistoryBrowserToMenu = (leaf: WorkspaceLeaf, submenu: Menu) => {
+		const { backHistory, forwardHistory } = leaf.history;
+		const { length } = backHistory;
+		const reversedForwardHistory = forwardHistory.slice().reverse();
+		backHistory.forEach((state, index) => {
+			submenu.addItem((item) => {
+				item.setTitle(state.title).setChecked(false);
+				item.onClick(() => leaf.history.go(index - length));
+			});
+		});
+		submenu.addItem((item) => {
+			item.setTitle(DeduplicatedTitle(app, leaf)).setChecked(true);
+		});
+		reversedForwardHistory.forEach((state, index) => {
+			submenu.addItem((item) => {
+				item.setTitle(state.title).setChecked(false);
+				item.onClick(() => leaf.history.go(index + 1));
+			});
+		});
+	};
+	const openHistoryInNewGroup = async () => {
+		// Make a copy of the current tab in a new group
+		const duplicatedLeaf = await workspace.duplicateLeaf(leaf, "split");
+		// Force it to be loaded
+		loadDeferredLeaf(duplicatedLeaf);
+		// Reset the new tab's history
+		duplicatedLeaf.history.backHistory = [];
+		duplicatedLeaf.history.forwardHistory = [];
+		// And open the tabs contained in the group
+		const group = duplicatedLeaf.parent;
+		const { backHistory, forwardHistory } = leaf.history;
+		let index = 0;
+		for (const state of backHistory) {
+			const leaf = workspace.createLeafInParent(group, index);
+			leaf.setViewState(state.state);
+			await loadDeferredLeaf(leaf);
+			leaf.setEphemeralState(state.eState);
+			index += 1;
+		}
+		index += 1; // Skip the current tab
+		for (const state of forwardHistory) {
+			const leaf = workspace.createLeafInParent(group, index);
+			leaf.setViewState(state.state);
+			await loadDeferredLeaf(leaf);
+			leaf.setEphemeralState(state.eState);
+			index += 1;
+		}
+		// Set the group title
+		const title = DeduplicatedTitle(app, leaf);
+		setGroupTitle(group.id, `History: ${title}`);
+		// Activate the new tab and lock the focus
+		workspace.setActiveLeaf(duplicatedLeaf, { focus: true });
+		lockFocusOnLeaf(app, duplicatedLeaf);
+	};
+
+	/* Commands - Zoom */
+	const addZoomOptionsToMenu = (
+		target: WorkspaceLeaf | BrowserView,
+		menu: Menu
+	) => {
+		const _ZoomIn =
+			target instanceof WorkspaceLeaf
+				? () => zoomIn(target.view)
+				: () => target.zoomIn();
+		const _ZoomOut =
+			target instanceof WorkspaceLeaf
+				? () => zoomOut(target.view)
+				: () => target.zoomOut();
+		const _ResetZoom =
+			target instanceof WorkspaceLeaf
+				? () => resetZoom(target.view)
+				: () => target.zoomReset();
+		menu.addItem((item) => item.setTitle("Zoom in").onClick(_ZoomIn));
+		menu.addItem((item) => item.setTitle("Zoom out").onClick(_ZoomOut));
+		menu.addItem((item) => item.setTitle("Reset zoom").onClick(_ResetZoom));
+	};
+
+	/* Commands - Webview */
+	const saveAsMarkdown = async (view: BrowserView) => {
+		const file = await view.saveAsMarkdown();
+		if (file) workspace.getLeaf("tab").openFile(file);
+	};
+
+	/* Effects */
+	// Bind and track the events that used for syncing with Obsidian,
+	// when the states are changed outside of the component.
+	useEffect(() => {
+		bindPinningEvent(leaf, setPinnedStates);
+		bindEphemeralToggleEvent(app, leaf, setIsEphemeral);
+	}, [leaf.id]);
+	// volatileTitle is used for syncing with the webview title.
+	useEffect(() => {
+		// When the tab is reused for a non-webview view, we need to reset it.
+		if (!isWebViewer) setVolatileTitle(null);
+		// If it's a webview, we update the volatile title whenever the page title is changed
+		if (Platform.isDesktop && isWebViewer) {
+			const view = leaf.view as BrowserView;
+			view.webview?.addEventListener(
+				"page-title-updated",
+				(data: { title: string }) => setVolatileTitle(data.title)
+			);
+		}
+	}, [isWebViewer]);
+
+	/* Menu */
+	const menu = new Menu();
+	// Bookmark
+	// TODO: Add customizable title support for bookmarks
 	menu.addItem((item) => {
 		item.setSection("bookmark")
 			.setTitle("Bookmark")
-			.onClick(() => {
-				createBookmarkForLeaf(app, leaf, leaf.getDisplayText());
-			});
+			.onClick(() =>
+				createBookmarkForLeaf(app, leaf, leaf.getDisplayText())
+			);
 	});
 	menu.addItem((item) => {
 		item.setSection("bookmark")
@@ -156,6 +284,7 @@ export const Tab = ({
 				leaf.detach();
 			});
 	});
+	// Show group view options when there's only one group and only tabs are visible
 	if (isSingleGroup && viewType) {
 		menu.addSeparator();
 		menu.addItem((item) => {
@@ -194,6 +323,7 @@ export const Tab = ({
 				);
 		});
 	}
+	// Tab control
 	menu.addSeparator();
 	menu.addItem((item) => {
 		item.setSection("close")
@@ -225,12 +355,14 @@ export const Tab = ({
 			.setDisabled(isPinned)
 			.onClick(() => leaf.parent.detach());
 	});
+	// Pinning
 	menu.addSeparator();
 	menu.addItem((item) => {
 		item.setSection("pin")
 			.setTitle(isPinned ? "Unpin" : "Pin")
 			.onClick(togglePinned);
 	});
+	// Workspace control
 	menu.addSeparator();
 	menu.addItem((item) => {
 		item.setSection("leaf")
@@ -259,6 +391,7 @@ export const Tab = ({
 				workspace.duplicateLeaf(leaf, "window");
 			});
 	});
+	// If the tab is navigatable, show history options
 	if (leaf.view.navigation && !alwaysOpenInNewTab) {
 		const historyLength =
 			leaf.history.backHistory.length +
@@ -279,30 +412,11 @@ export const Tab = ({
 		menu.addItem((item) => {
 			item.setSection("history").setTitle("Browse history");
 			if (Platform.isDesktop) {
+				// On desktop, we show tab history in a submenu
 				const submenu = item.setSubmenu();
-				const { backHistory, forwardHistory } = leaf.history;
-				const { length } = backHistory;
-				backHistory.forEach((state, index) => {
-					submenu.addItem((item) => {
-						item.setTitle(state.title).setChecked(false);
-						item.onClick(() => leaf.history.go(index - length));
-					});
-				});
-				submenu.addItem((item) => {
-					item.setTitle(DeduplicatedTitle(app, leaf)).setChecked(
-						true
-					);
-				});
-				forwardHistory
-					.slice()
-					.reverse()
-					.forEach((state, index) => {
-						submenu.addItem((item) => {
-							item.setTitle(state.title).setChecked(false);
-							item.onClick(() => leaf.history.go(index + 1));
-						});
-					});
+				addHistoryBrowserToMenu(leaf, submenu);
 			} else {
+				// On mobile, we show it as a modal (submenus are not supported)
 				item.setDisabled(historyLength === 0);
 				item.onClick(() => new HistoryBrowserModal(app, leaf).open());
 			}
@@ -317,37 +431,7 @@ export const Tab = ({
 			item.setSection("history")
 				.setTitle("Open history in new group")
 				.setDisabled(historyLength === 0)
-				.onClick(async () => {
-					const duplicatedLeaf = await workspace.duplicateLeaf(
-						leaf,
-						"split"
-					);
-					loadDeferredLeaf(duplicatedLeaf);
-					duplicatedLeaf.history.backHistory = [];
-					duplicatedLeaf.history.forwardHistory = [];
-					const group = duplicatedLeaf.parent;
-					const { backHistory, forwardHistory } = leaf.history;
-					let index = 0;
-					for (const state of backHistory) {
-						const leaf = workspace.createLeafInParent(group, index);
-						leaf.setViewState(state.state);
-						await loadDeferredLeaf(leaf);
-						leaf.setEphemeralState(state.eState);
-						index += 1;
-					}
-					index += 1;
-					for (const state of forwardHistory) {
-						const leaf = workspace.createLeafInParent(group, index);
-						leaf.setViewState(state.state);
-						await loadDeferredLeaf(leaf);
-						leaf.setEphemeralState(state.eState);
-						index += 1;
-					}
-					const title = DeduplicatedTitle(app, leaf);
-					setGroupTitle(group.id, `History: ${title}`);
-					workspace.setActiveLeaf(duplicatedLeaf, { focus: true });
-					lockFocusOnLeaf(app, duplicatedLeaf);
-				});
+				.onClick(openHistoryInNewGroup);
 		});
 		menu.addItem((item) => {
 			item.setSection("history")
@@ -360,6 +444,7 @@ export const Tab = ({
 				});
 		});
 	}
+	// Placeholder for deferred (inactive) tabs
 	if (isDeferredLeaf(leaf) && !alwaysOpenInNewTab) {
 		menu.addSeparator();
 		menu.addItem((item) => {
@@ -371,95 +456,54 @@ export const Tab = ({
 				.onClick(async () => await loadDeferredLeaf(leaf));
 		});
 	}
-	if (enableTabZoom && !isWebViewer) {
+	// Per-tab zoom
+	if (enableTabZoom) {
 		menu.addSeparator();
 		if (Platform.isDesktop) {
+			// On desktop, we show zoom options in a submenu
 			menu.addItem((item) => {
 				item.setSection("zoom").setTitle("Zoom");
 				const submenu = item.setSubmenu();
-				submenu.addItem((item) => {
-					item.setTitle("Zoom in").onClick(() => {
-						zoomIn(leaf.view);
-					});
-				});
-				submenu.addItem((item) => {
-					item.setTitle("Zoom out").onClick(() => {
-						zoomOut(leaf.view);
-					});
-				});
-				submenu.addItem((item) => {
-					item.setTitle("Reset zoom").onClick(() => {
-						resetZoom(leaf.view);
-					});
-				});
+				if (isWebViewer) {
+					const view = leaf.view as BrowserView;
+					addZoomOptionsToMenu(view, submenu);
+				} else {
+					addZoomOptionsToMenu(leaf, submenu);
+				}
 			});
 		} else {
-			menu.addItem((item) => {
-				item.setTitle("Zoom in").onClick(() => {
-					zoomIn(leaf.view);
-				});
-			});
-			menu.addItem((item) => {
-				item.setTitle("Zoom out").onClick(() => {
-					zoomOut(leaf.view);
-				});
-			});
-			menu.addItem((item) => {
-				item.setTitle("Reset zoom").onClick(() => {
-					resetZoom(leaf.view);
-				});
-			});
+			// On mobile, we add them directly to the parent menu (submenus are not supported)
+			addZoomOptionsToMenu(leaf, menu);
 		}
 	}
-	if (Platform.isDesktop && isWebViewer) {
-		const webview = leaf.view as BrowserView;
-		if (webview.webview) {
-			webview.webview.addEventListener(
-				"page-title-updated",
-				(data: { title: string }) => setVolatileTitle(data.title)
-			);
-		}
-		menu.addSeparator();
-		menu.addItem((item) => {
-			item.setSection("webview")
-				.setTitle("Toggle reader mode")
-				.onClick(() => webview.toggleReaderMode());
-		});
-		menu.addItem((item) => {
-			item.setSection("webview")
-				.setTitle("Save to vault")
-				.onClick(async () => {
-					const file = await webview.saveAsMarkdown();
-					if (file) workspace.getLeaf("tab").openFile(file);
-				});
-		});
-		menu.addSeparator();
-		menu.addItem((item) => {
-			item.setSection("zoom").setTitle("Zoom");
-			const submenu = item.setSubmenu();
-			submenu.addItem((item) => {
-				item.setTitle("Zoom in").onClick(() => webview.zoomIn());
-			});
-			submenu.addItem((item) => {
-				item.setTitle("Zoom out").onClick(() => webview.zoomOut());
-			});
-			submenu.addItem((item) => {
-				item.setTitle("Reset zoom").onClick(() => webview.zoomReset());
-			});
-		});
-	}
-	if (Platform.isDesktop && !isWebViewer) {
+	// Show tab options
+	if (Platform.isDesktop) {
 		menu.addSeparator();
 		menu.addItem((item) => {
 			item.setSection("more").setTitle("More options");
 			const submenu = item.setSubmenu();
-			leaf.view.onPaneMenu(submenu, "more-options");
-			const excludedSections = ["open", "find", "pane"];
-			submenu.items = submenu.items.filter(
-				(item) =>
-					item.section === undefined ||
-					!excludedSections.includes(item.section)
-			);
+			if (isWebViewer) {
+				const webview = leaf.view as BrowserView;
+				submenu.addItem((item) => {
+					item.setSection("webview")
+						.setTitle("Toggle reader mode")
+						.onClick(() => webview.toggleReaderMode());
+				});
+				submenu.addItem((item) => {
+					item.setSection("webview")
+						.setTitle("Save to vault")
+						.onClick(() => saveAsMarkdown(webview));
+				});
+			} else {
+				// For non-webview tabs, we copy those provided by Obsidian
+				leaf.view.onPaneMenu(submenu, "more-options");
+				const excludedSections = ["open", "find", "pane"];
+				submenu.items = submenu.items.filter(
+					(item) =>
+						item.section === undefined ||
+						!excludedSections.includes(item.section)
+				);
+			}
 		});
 	}
 
@@ -484,11 +528,6 @@ export const Tab = ({
 			)}
 		</Fragment>
 	);
-
-	const props = {
-		icon: leaf.getIcon(),
-		isActive: leaf.tabHeaderEl?.classList.contains("is-active"),
-	};
 
 	const [webviewIcon, setWebviewIcon] = useState<string | undefined>();
 
@@ -577,21 +616,6 @@ export const Tab = ({
 		}
 	}, [viewCueIndex, ref]);
 
-	const previewTab = (
-		event: React.MouseEvent<HTMLDivElement, MouseEvent>
-	) => {
-		const file = getOpenFileOfLeaf(app, leaf);
-		if (file && ref.current) {
-			workspace.trigger("hover-link", {
-				event: event.nativeEvent,
-				source: "vertical-tabs",
-				hoverParent: leaf,
-				targetEl: ref.current,
-				linktext: file.path,
-			});
-		}
-	};
-
 	return (
 		<NavigationTreeItem
 			ref={ref}
@@ -611,7 +635,8 @@ export const Tab = ({
 			dataType={leaf.getViewState().type}
 			dataId={leaf.id}
 			webviewIcon={webviewIcon}
-			{...props}
+			icon={leaf.getIcon()}
+			isActive={leaf.tabHeaderEl?.classList.contains("is-active")}
 			{...listeners}
 		/>
 	);
