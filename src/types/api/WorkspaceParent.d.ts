a2e90d9e158455edf95920c07608dc4cf48026fb
oxdc <29519076+oxdc@users.noreply.github.com>
2025-08-12 17:43:57 +0800

    use Obsidian's default impl for moving tabs

--- src/types/api/WorkspaceParent.d.ts
+++ src/types/api/WorkspaceParent.d.ts
@@ -5,6 +5,8 @@ declare module "obsidian" {
 		tabsContainerEl: HTMLElement;
 		currentTab: number;
 		children: WorkspaceLeaf[];
+		removeChild: (leaf: WorkspaceLeaf) => void;
+		insertChild: (index: number, leaf: WorkspaceLeaf) => void;
 		selectTab(leaf: WorkspaceLeaf): void;
 		selectTabIndex(index: number): void;
 		recomputeChildrenDimensions(): void;
