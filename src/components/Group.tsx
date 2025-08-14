669b27150e002d77f0b374ded34aa4c2f1e08da5
oxdc <29519076+oxdc@users.noreply.github.com>
2025-08-14 11:53:16 +0800

    fixed #175

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -83,8 +83,8 @@ export const Group = (props: GroupProps) => {
 	const isCollapsed =
 		(!!group && collapsedGroups.includes(group.id)) ||
 		(isSidebar && collapsedGroups.includes(type));
-	const isSingleGroup =
-		hasOnlyOneGroup() && hideSidebars && !isSidebar && !!group;
+	const isSingleGroupInRoot = hasOnlyOneGroup() && !isSidebar && !!group;
+	const isSingleGroupInView = hideSidebars && isSingleGroupInRoot;
 	const isActiveGroup = group?.id === lastActiveLeaf?.parent?.id;
 	const hasMore =
 		!!linkedFolder && linkedFolder.files.length > linkedFolder.offset;
@@ -100,10 +100,16 @@ export const Group = (props: GroupProps) => {
 		const modifiedID = isSidebar ? type : group.id;
 		toggleCollapsedGroup(modifiedID, !isCollapsed);
 	};
-	const toggleHidden = () => {
+	const setHidden = (hidden: boolean) => {
 		if (isSidebar || !group) return;
-		toggleHiddenGroup(group.id, !isHidden);
+		toggleHiddenGroup(group.id, hidden);
 		workspace.trigger(EVENTS.UPDATE_TOGGLE);
+	};
+	const toggleHidden = () => {
+		setHidden(!isHidden);
+	};
+	const unhideGroup = () => {
+		setHidden(false);
 	};
 	/* Commands - Title */
 	const startEditing = () => {
@@ -172,9 +178,11 @@ export const Group = (props: GroupProps) => {
 	});
 	// Manage the visibility of the group
 	useEffect(() => {
+		// Automatically unhide the group if it's the only one in the root split (#175)
+		if (isSingleGroupInRoot) unhideGroup();
 		group?.containerEl.toggleClass("is-hidden", isHidden);
 		return () => group?.containerEl.removeClass("is-hidden");
-	}, [isHidden]);
+	}, [isHidden, isSingleGroupInRoot]);
 	// Bind and track the events that used for syncing with Obsidian,
 	// when the states are changed outside of the component.
 	useEffect(() => {
@@ -388,9 +396,9 @@ export const Group = (props: GroupProps) => {
 			dataType={type}
 			toolbar={toolbar}
 			icon="right-triangle"
-			isCollapsed={isCollapsed && !isSingleGroup} // Single group should not be collapsed
+			isCollapsed={isCollapsed && !isSingleGroupInView} // Single group should not be collapsed
 			isSidebar={isSidebar}
-			isSingleGroup={isSingleGroup}
+			isSingleGroup={isSingleGroupInView}
 			isActiveGroup={isActiveGroup}
 		>
 			{!!linkedFolder && (
@@ -400,7 +408,7 @@ export const Group = (props: GroupProps) => {
 					onClick={unlinkGroup}
 				/>
 			)}
-			{children && children(isSingleGroup, viewType)}
+			{children && children(isSingleGroupInView, viewType)}
 			{hasMore && (
 				<LinkedGroupButton
 					title="Load more"
